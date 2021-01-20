f9bd1768c65cc11f084db0b8531acb0f65a4f25f
Lucas Vogel <contact@vogelino.com>
2021-01-20 22:10:44 +0100

    Add cors on the createHandler Method

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,15 +3,16 @@ import typeDefs from '../../graphql/types';
 import resolvers from '../../graphql/resolvers';
 
 const server = new ApolloServer({
-	cors: {
-		origin: '*',
-		credentials: true
-	},
 	typeDefs,
 	resolvers,
 	introspection: true,
 	playground: true,
 });
 
-export default server.createHandler();
+export default server.createHandler({
+	cors: {
+		origin: '*',
+		credentials: true
+	},
+});
 
